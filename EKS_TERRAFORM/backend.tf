7d269de10cd73066fe31c00819131e5d09244e04
dimili2212 <135544813+dimili2212@users.noreply.github.com>
2025-08-05 16:47:42 +0100

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-netflix-dimili-143" # Replace with your actual S3 bucket name
+    bucket = "devsecops-netflix-dimili-22" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "eu-north-1"
   }
